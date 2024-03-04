3f5ad626d6d54de52c7c66944b1aff7626b55459
Ignacio Barraza Rioja <ignacio.barraza.rioja@gmail.com>
2024-03-03 23:52:09 -0300

    feat: :sparkles: Config base project, install base dependencies, config firebase, add base components

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,21 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { environment } from '../environments/environment';
+import { AngularFireModule } from "@angular/fire/compat";
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom([
+      provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+      provideDatabase(() => getDatabase()),
+      AngularFireModule.initializeApp(environment.firebaseConfig)
+    ]), provideAnimationsAsync(),
+    
+  ],
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: "AIzaSyAnmg7l1BBVZnZfhFyDT4MfOxHPPYHGNhE",
+    authDomain: "osmangular-b683b.firebaseapp.com",
+    databaseURL: "https://osmangular-b683b-default-rtdb.firebaseio.com",
+    projectId: "osmangular-b683b",
+    storageBucket: "osmangular-b683b.appspot.com",
+    messagingSenderId: "598444778820",
+    appId: "1:598444778820:web:f955d69b5ef309ee462311",
+    measurementId: "G-603YEK1GKX"
+  }
+};
