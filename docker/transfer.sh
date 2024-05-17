b0dfe514eec72db479070aebf3efd48afae4c4f3
Ignacio Barraza Rioja <ignacio.barraza.rioja@gmail.com>
2024-05-17 18:52:51 -0400

    👷 update transfer.sh file

--- docker/transfer.sh
+++ docker/transfer.sh
@@ -3,12 +3,20 @@
 # Variables (modify as needed)
 REMOTE_USER=nacho
 REMOTE_HOST=192.168.1.85
-REMOTE_PATH=/home/nacho/Desktop/tech-challenge
+REMOTE_TEMP_PATH=/home/nacho/Desktop/tech-challenge
+REMOTE_FINAL_PATH=/var/www/tech-challenge
 LOCAL_PATH=/transfer/dist/desafio-tecnico-lazarillo/
 PASSWORD=${PASSWORD}
 
-# Ensure the remote path exists
-sshpass -p "${PASSWORD}" ssh -o StrictHostKeyChecking=no ${REMOTE_USER}@${REMOTE_HOST} "mkdir -p ${REMOTE_PATH}"
+# Ensure the remote temp path exists
+sshpass -p "${PASSWORD}" ssh -o StrictHostKeyChecking=no ${REMOTE_USER}@${REMOTE_HOST} "mkdir -p ${REMOTE_TEMP_PATH}"
 
-# Copy files to the remote machine
-sshpass -p "${PASSWORD}" scp -o StrictHostKeyChecking=no -r ${LOCAL_PATH}/* ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH}
+# Copy files to the remote temp directory
+sshpass -p "${PASSWORD}" scp -o StrictHostKeyChecking=no -r ${LOCAL_PATH}/* ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_TEMP_PATH}
+
+# Move files to the final destination with sudo
+sshpass -p "${PASSWORD}" ssh -o StrictHostKeyChecking=no ${REMOTE_USER}@${REMOTE_HOST} "
+echo ${PASSWORD} | sudo -S rm -rf ${REMOTE_FINAL_PATH} &&
+echo ${PASSWORD} | sudo -S mkdir -p ${REMOTE_FINAL_PATH}
+echo ${PASSWORD} | sudo -S cp -r ${REMOTE_TEMP_PATH}/* ${REMOTE_FINAL_PATH} && 
+echo ${PASSWORD} | sudo -S rm -rf ${REMOTE_TEMP_PATH}/*"
